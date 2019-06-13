32c850f68fd01d088de1f97e828aa6579d3a3cae
chinitacode <ziyu_zhou_victoria@163.com>
2019-06-13 15:30:57 +0800

    update HelloNumbers.java

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,13 +1,15 @@
-public class HelloNumbers{
-	public static void main(String[] args){
-		int x = 0;
-		int sum = 0;
-		while (x < 10) {
-			sum = sum + x;
-			System.out.println(sum);
-			x = x + 1;
-		}
-		System.out.println(5 + 10);
-		System.out.println(5 + "10");
-	}
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        String y = 4 + "horse";
+        while (x < 10) {
+            sum += x;
+            System.out.print(sum + " ");
+            System.out.print(3 + "10");
+            x = x + 1;
+
+        }
+        System.out.println(y);
+    }
 }
